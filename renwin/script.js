0de5ba458c967ab3e9d4c9086426aab5845c0159
kzshtr <kzshtr3@mail.ru>
2025-05-01 12:48:09 +0300

    Add files via upload

---
+++ renwin/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const telegramButton = document.getElementById('telegramButton');
+
+    if (telegramButton) {
+        const telegramUsername = 'recrut55'; // Убедитесь, что это ваше имя пользователя
+        const telegramUrl = `https://t.me/${telegramUsername}`;
+
+        telegramButton.addEventListener('click', () => {
+            window.open(telegramUrl, '_blank'); // Открывает в новой вкладке
+        });
+    } else {
+        console.error('Кнопка Telegram с ID "telegramButton" не найдена.');
+    }
+});
