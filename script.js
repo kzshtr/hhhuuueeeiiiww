c640652f4bddfded304091fcaff608bd8147f324
kzshtr <kzshtr3@mail.ru>
2025-05-01 13:07:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const telegramButton = document.getElementById('telegramButton');
 
     if (telegramButton) {
-        const telegramUsername = 'recrut55'; // Убедитесь, что это ваше имя пользователя
         const telegramUrl = `https://t.me/tripX_bot`;
 
         telegramButton.addEventListener('click', () => {
